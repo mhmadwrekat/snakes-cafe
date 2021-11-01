e7c214b429350004ce519d7a16dbf87a98592083
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-11-01 23:55:49 +0200

    edit and finish

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,84 +1,61 @@
-##
-order_in = ""
-def in_n_out():
-    order_in = input("> ")
-    if order_in == "quit" :
-        pass
 
 
-def format_text():
-    print(
-        "**************************************\r\n",
-        "**    Please see our menu below.    **\r\n",
-        "**    Welcome to the Snakes Cafe!   **\r\n **\r\n",
-        '** To quit at any time, type "quit" **\r\n',
-        "**************************************\r\n"
-    )
-    print(" Appetizers\r\n",
-    "----------\r\n",
-    "Wings\r\n",
-    "Cookies\r\n",
-    "Spring Rolls\r\n\r\n",
+menu = {
+    "Appetizers":["Wings","Cookies","Spring Rolls"],
+    "Entrees":["Salmon","Steak","Meat Tornado","A Literal Garden"],
+    "Desserts":["Ice Cream","Cake","Pie"],
+    "Drinks":["Coffee","Tea","Unicorn Tears"],
+}
 
-    " Entrees\r\n",
-    "-------\r\n",
-    "Salmon\r\n",
-    "Steak\r\n",
-    "Meat Tornado\r\n",
-    "A Literal Garden\r\n\r\n",
+def menuIntrod():
+    print('**************************************')
+    print('**    Welcome to the Snakes Cafe!   **')
+    print('**    Please see our menu below.    **')
+    print('**')
+    print('** To quit at any time, type "quit" **')
+    print('**************************************\n')
+    print('')
+    catego = menu.items()
+    print(catego)
+    for key, value in catego:
+      print(key)
+      print('-'*len(key))
+      print(value)
+      for dish in value:
+        print(dish)
+      print('\n')
 
-    " Desserts\r\n",
-    "--------\r\n",
-    "Ice Cream\r\n",
-    "Cake\r\n",
-    "Pie\r\n\r\n",
 
-    " Drinks\r\n",
-    "------\r\n",
-    "Coffee\r\n",
-    "Tea\r\n",
-    "Unicorn Tears\r\n\r\n",
+def Order():
+    count=0
+    count_2=0
+    print('***********************************')
+    print('** What would you like to order? **')
+    print('***********************************\n')
+    order = input('> ')
+    print('\n')
+    dish = order
+    while order:
+      if order == 'quit':
+        Exit()
+      else:
+        if dish == order:
+            count+=1
+            print(f'** {count} order of {order} have been added to your meal **\n')
+        else:
+            dish_2=order
+            count_2+=1
+            print(f'** {count_2} order of {order} have been added to your meal **\n')
+        order=''
+        order = input('> ')
+        print('\n')
+                   
+def Exit():
+    exitInput=input('Exit the program ? quit ( y / n ) \n > ')
+    if exitInput=='y':
+        exit()
+    else:
+        Order()
 
-    "***********************************\r\n",
-    "** What would you like to order? **\r\n",
-    "***********************************\r\n",)
-    in_n_out()
-    
-menu = [
-    {
-        "name": "Appetizers",
-        "types": [
-            "Wings",
-            "Cookies",
-            "Spring Rolls",
-        ],
-    },
-    {
-        "name": "Entrees",
-        "types": [
-            "Salmon",
-            "Steak",
-            "Meat Tornado",
-            "A Literal Garden",
-        ],
-    },
-        {
-        "name": "Desserts",
-        "types": [
-            "Ice Cream",
-            "Cake",
-            "Pie",
-        ],
-    },
-            {
-        "name": "Drinks",
-        "types": [
-            "Coffee",
-            "Tea",
-            "Unicorn Tears",
-        ],
-    },
-]
-
-if __name__=="__main__":
-    format_text()
+menuIntrod()
+Order()
